a3a01663518cbbd847268c53e73fe7dc2414989f
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-04-20 18:52:12 +0100

    New URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://arisamiga.github.io/Examinations', // Set your GitHub Pages URL
-    base: '/Examinations/', // Set the base path to your repository name
-  });
+    site: 'https://examinations.arisamiga.rocks/',
+    base: '/',
+});
